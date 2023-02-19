76d0cb7de028b1e8de7d2f2eab3a0797821d6f7b
VelmaMartha <marthaauma3508@gmail.com>
2023-02-19 00:36:43 -0800

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include<stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -Entry point
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int intType;
+	float floatType;
+	double doubleTpe;
+	char charType;
+}		printf ("size of int:%d bytes\n",(long),sizeof (intType));
+		printf ("size of float:%d bytes\n",sizeof (floatType));
+		printf ("size of double:%d bytes\n",sizeof (doubleType));
+		printf ("size of char:%d byte\n",sizeof (charType));
+		printf("size of int:%d bytes\n",(long long),sizeof(intType));
+}
+	return (0);
+}
+
