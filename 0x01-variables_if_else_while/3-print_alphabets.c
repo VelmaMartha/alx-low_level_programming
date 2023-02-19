df049cba989784e049f2b851b28ad116656fb495
VelmaMartha <marthaauma3508@gmail.com>
2023-02-19 12:18:09 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *
+ *main -Entry point
+ *
+ *Return:Always
+ *
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	for (n = 97; n <= 122; n++)
+	putchar (n);
+
+{
+	for (x = 65; x <= 90; x++)
+	putchar (x);
+}
+	putchar ('\n');
+	return (0);
+}
