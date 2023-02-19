122177ec683e02c97b46200e992451d2c2325c2f
VelmaMartha <marthaauma3508@gmail.com>
2023-02-19 14:55:21 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *Return: Always 0.
+ *
+ */
+int main(void)
+{
+	int x;
+	int m;
+
+	for (x = 48; x <= 57; x++)
+	putchar(x);
+	{
+	for (m = 97; m <= 102; m++)
+	putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
+
