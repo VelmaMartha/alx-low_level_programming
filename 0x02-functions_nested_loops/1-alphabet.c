0d74bece30205752b580bd7dd382c7ae48bb5b23
VelmaMartha <marthaauma3508@gmail.com>
2023-02-22 14:53:56 -0800

    printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets
+ *
+ * Return: void
+ */
+void print_alphabet()
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+	_putchar('x');
+	}
+	_putchar('\n');
+}
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar(int char);
+void print_alphabet(void);
