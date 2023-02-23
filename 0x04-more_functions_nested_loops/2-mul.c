95b470254705e188d9ae8adccb6fd1307c6fbf36
VelmaMartha <marthaauma3508@gmail.com>
2023-02-23 11:48:33 -0800

    multiplies two integers.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *mul - multiplies two integers
- *@ a: number to be multiplied
- *@ b: number to be multiplied also
- * Return: mult
+ *@a: number to be multiplied
+ *@b: number to be multiplied also
+ *Return: mult
  */
 int mul(int a, int b)
 {
 	int mult;
 
 	mult = a * b;
-	return mult;
+	return (mult);
 }
