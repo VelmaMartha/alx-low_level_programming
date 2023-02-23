b2fb4d0c239cd6c76cbe217f050102403dab1bde
VelmaMartha <marthaauma3508@gmail.com>
2023-02-22 16:31:31 -0800

    printing ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *
  * Return: void
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int x;
 	int i;
@@ -15,5 +15,7 @@ void print_alphabet_x10(void);
 		{	
 		putchar(x);
 		}
+		putchar('\n')
 		}
+
 }
