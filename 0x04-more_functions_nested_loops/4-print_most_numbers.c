bb14a86b9733a8904c97e3b4213faddceeabfd5f
VelmaMartha <marthaauma3508@gmail.com>
2023-02-25 14:29:04 -0800

    printing most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,12 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 48; c < 57; c++)
+	for (c = 48; c <= 57; c++)
 	{
 		if (c != 50 && c != 52)
-	{
-		_putchar (c);
+		{
+			_putchar(c);
+		}
 	}
-	}
-	_putchar ('\n')
+	_putchar('\n');
 }
-
