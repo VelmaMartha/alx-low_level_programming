8a5d1efc1e297db8bdc2fbd4d8075dc66ce67858
VelmaMartha <marthaauma3508@gmail.com>
2023-03-07 11:40:47 -0800

    strchar

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsighned int;
 
-	for (; n > 0; i++)
+	i = 0
+	for (i = 0; i < n;)
 	{
-		s[i] = b;
+		s
 		n--;
 	}
 	return (s);
