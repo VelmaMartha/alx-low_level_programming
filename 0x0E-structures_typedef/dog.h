b0ca7a875bc1c59b6d0691c79fe7496044114329
VelmaMartha <marthaauma3508@gmail.com>
2023-03-21 18:48:07 -0700

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct my_struct - Short description
+ * struct dog - Short description
  * @name: name of a dog
  * @age: age of a dog
  * @owner: owner of a dog
